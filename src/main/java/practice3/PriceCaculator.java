a698c660be9cb8256190c95623dc79c291e23373
Galileo2010 <15549070600@163.com>
2019-03-27 14:45:32 +0800

    refactor Order

--- src/main/java/practice3/PriceCaculator.java
+++ src/main/java/practice3/PriceCaculator.java
@@ -1,4 +1,28 @@
 package practice3;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 public class PriceCaculator {
+    public static BigDecimal calculate(List<OrderLineItem> orderLineItemList, List<BigDecimal> discounts, BigDecimal tax) {
+        BigDecimal subTotal = new BigDecimal(0);
+
+        // Total up line items
+        for (OrderLineItem lineItem : orderLineItemList) {
+            subTotal = subTotal.add(lineItem.getPrice());
+        }
+
+        // Subtract discounts
+        for (BigDecimal discount : discounts) {
+            subTotal = subTotal.subtract(discount);
+        }
+
+        // calculate tax
+        BigDecimal _tax = subTotal.multiply(tax);
+
+        // calculate GrandTotal
+        BigDecimal grandTotal = subTotal.add(_tax);
+
+        return grandTotal;
+    }
 }
